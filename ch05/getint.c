d8ea109dcc1b70c57d3e43ba5b939e3009832bce
Lien Cao <lienctm@gmail.com>
2018-06-16 21:17:29 -0700

    revise 1

--- ch05/getint.c
+++ ch05/getint.c
@@ -8,6 +8,37 @@
 
 
 #include<stdio.h>
+#include<ctype.h>
+#define SIZE 100
 
 int main() {
+  int n, array[SIZE], getint(int *);
+  for( n = 0; n< SIZE && getint(&array[n]) != EOF; n++);
+
+  int i;
+  for( i = 0; i < n; i++ )
+    printf("%d\t", array[i]);
+  printf(" \n");
 }
+
+int getch(void);
+void ungetch(int);
+
+int getint(int *pn)
+{
+  int c, sign;
+
+  while(isspace(c = getch())); /* get until c is not space */
+  if( !isdigit(c) && c != EOF && c != '-' && c != '+' ) {
+    ungetch(c);
+    return 0;
+  }
+  sign = ( c == '-' ) ? -1 : 1;
+  for( *pn = 0; isdigit(c); c = getch() )
+    *pn = *pn * 10 + (c - '0');
+  *pn *= sign;
+  if( c != EOF )
+    ungetch(c);
+  return c;
+}
+
