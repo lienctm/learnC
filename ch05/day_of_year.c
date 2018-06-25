b0686dd9439d2d1823681d4a8c1ac2674710ec7b
Lien Cao <lienctm@gmail.com>
2018-06-25 16:41:15 -0700

    fixed

--- ch05/day_of_year.c
+++ ch05/day_of_year.c
@@ -11,7 +11,7 @@ static char daytab[2][13] = {
 int day_of_year(int year, int month, int day) {
   int i, leap;
 
-  (leap = year%4 == 0 && year%100 != 0) || year%400 == 0;
+  leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
   for(i = 1; i < month; i++)
     day += daytab[leap][i];
   return day;
