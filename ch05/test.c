d46aa7957050038f1adedd7d3bebb88faaa2656c
Lien Cao <lienctm@gmail.com>
2018-07-30 23:12:46 -0700

    array and pointer

--- ch05/test.c
+++ ch05/test.c
@@ -1,6 +1,19 @@
+ /* Purpose : want to check the different size of array and pointer */
+
 #include <stdio.h>
 
 int main() {
-  printf("hello wolrd \n");
-  return 0;
+  int a[15];      /* a: array[15] of int */
+  printf("size of a[15] is %d\n", sizeof(a));
+
+  int (*b)[15];   /* b: pointer to array[15] of int */
+  //int *b = a;    // OK;
+  printf("size of *b is %d\n", sizeof(b));
+
+  char c[15];
+  printf("size of c[] is %d\n", sizeof(c));
+
+  char *d;
+  printf("size of *d is %d\n", sizeof(d));
 }
+
